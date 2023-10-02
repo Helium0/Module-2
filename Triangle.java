ca992ab5bde4080d7f5749648dc35a7d9a0549d6
Patryk Prentki <patryk.prentki90@gmail.com>
2023-10-03 00:01:27 +0200

    Added triangle program

---
+++ Triangle.java
@@ -0,0 +1,31 @@
+
+
+
+public class Triangle {
+    public static void main(String[] args) {
+   Triangle newnum = new Triangle();
+      newnum.TriangleFunction(7);
+    }
+       
+    
+    
+    public static void TriangleFunction(int number){
+    String triangle = "*";
+    System.out.println(triangle);
+    
+    for( int i=0; i<number; i++){
+        triangle = triangle +"*";
+        if(i>number){
+    }System.out.println(triangle);
+    }
+}
+}
+   
+        
+
+
+    
+
+  
+        
+
