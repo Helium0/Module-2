6af9c44a63d7e64e8b81529a8ebe17f1e93f837e
Patryk Prentki <patryk.prentki90@gmail.com>
2023-10-12 01:56:22 +0200

    Added another exercise: sum numbers method from Table

---
+++ SumTable.java
@@ -0,0 +1,20 @@
+
+public class SumTable {
+    public static void main(String[] args) {
+        SumTable table = new SumTable();
+        int[] tabloid = {1,4,12,15,7,19};
+        table.sumArray(tabloid);
+           
+    }
+        
+    public int sumArray(int[] numbers){
+        int sum =0 ;
+        for( int i=0; i<numbers.length; i++){           
+            int el = numbers[i];
+            sum = numbers[i]+sum ;
+        } System.out.println ("Table sum is: "+ sum); 
+            return 0;
+        }
+        
+    }
+        
