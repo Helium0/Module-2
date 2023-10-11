1f96036d622f3aa92a4e2a6d034b467823f6c3c2
Patryk Prentki <patryk.prentki90@gmail.com>
2023-10-12 01:59:20 +0200

    Fixed sum numbers method from Table

--- SumTable.java
+++ SumTable.java
@@ -2,15 +2,14 @@
 public class SumTable {
     public static void main(String[] args) {
         SumTable table = new SumTable();
-        int[] tabloid = {1,4,12,15,7,19};
+        int[] tabloid = {1,4,12,15,33,19};
         table.sumArray(tabloid);
            
     }
         
     public int sumArray(int[] numbers){
         int sum =0 ;
-        for( int i=0; i<numbers.length; i++){           
-            int el = numbers[i];
+        for( int i=0; i<numbers.length; i++){                  
             sum = numbers[i]+sum ;
         } System.out.println ("Table sum is: "+ sum); 
             return 0;
